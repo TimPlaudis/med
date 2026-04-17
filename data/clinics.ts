1cbb2513fa9d2bf54ec4848bb79a1cdf66c8aaeb
TimPlaudis <nvk97174@gmail.com>
2026-04-17 13:48:11 +0600

    Create clinics.ts

---
+++ data/clinics.ts
@@ -0,0 +1,29 @@
+export const clinics = [
+  {
+    id: 1,
+    name: "City Clinic Astana",
+    address: "Mangilik El 10",
+    type: "private",
+    category: "clinic",
+    lat: 51.128,
+    lng: 71.430,
+  },
+  {
+    id: 2,
+    name: "State Hospital №1",
+    address: "Abay 12",
+    type: "public",
+    category: "hospital",
+    lat: 51.135,
+    lng: 71.420,
+  },
+  {
+    id: 3,
+    name: "Dent Lux",
+    address: "Turkestan 5",
+    type: "private",
+    category: "dentistry",
+    lat: 51.122,
+    lng: 71.445,
+  },
+];
