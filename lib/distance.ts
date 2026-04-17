d7f76a6df7a0ca340879e388d1026707b291f00b
TimPlaudis <nvk97174@gmail.com>
2026-04-17 13:58:42 +0600

    Create distance.ts

---
+++ lib/distance.ts
@@ -0,0 +1,26 @@
+export function getDistance(
+  lat1: number,
+  lon1: number,
+  lat2: number,
+  lon2: number
+): number {
+  const R = 6371; // радиус Земли в километрах
+
+  const toRad = (value: number) => (value * Math.PI) / 180;
+
+  const dLat = toRad(lat2 - lat1);
+  const dLon = toRad(lon2 - lon1);
+
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(toRad(lat1)) *
+      Math.cos(toRad(lat2)) *
+      Math.sin(dLon / 2) *
+      Math.sin(dLon / 2);
+
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+  const distance = R * c;
+
+  return Number(distance.toFixed(2)); // округление до 2 знаков
+}
